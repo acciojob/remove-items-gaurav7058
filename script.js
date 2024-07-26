ffe377c78713461cabd4ddc3a5668cbf63be0d47
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-07-26 18:42:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let colorList=document.getElementById("colorSelect");
+
+colorList.addEventListener("click",()=>{
+	for (let i = 0; i < colorList.length; i++) {
+		colorList[i].remove();
+	}
+})  
